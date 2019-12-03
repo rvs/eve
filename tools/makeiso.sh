7326f94370eff905c74064ac7d2456f0c16fe15e
Roman Shaposhnik <rvs@zededa.com>
2019-12-03 11:15:02 -0800

    Making iso disk generation use parts

--- tools/makeiso.sh
+++ tools/makeiso.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 MKIMAGE_TAG="$(linuxkit pkg show-tag pkg/mkimage-iso-efi)"
-PARTS="$(cd $1 && pwd)"
+SOURCE="$(cd $1 && pwd)"
 ISO="$(cd $(dirname $2) && pwd)/$(basename $2)"
 
-if [ ! -d "$PARTS" -o $# -ne 2 ]; then
+if [ ! -d "$SOURCE" -o $# -ne 2 ]; then
    echo "Usage: $0 <input dir> <output iso image file>"
    exit 1
 fi
 
-touch "$PARTS/../boot.img" "$2"
-docker run -t -v "$PARTS:/mnt" -v "$PARTS/../boot.img:/mnt/boot.img" -v "$ISO:/output.iso" -i ${MKIMAGE_TAG}
+docker run -t -v "$SOURCE:/bits" -v "$ISO:/output.iso" -i ${MKIMAGE_TAG}
